3f4f9b34d9efa36cbd3239e6ceff94e15e41a3ef
Jorge Aparicio <jorge@japaric.io>
2017-05-12 09:47:48 -0500

    install ARM toolchain using sudo apt-get

--- ci/install.sh
+++ ci/install.sh
@@ -4,7 +4,9 @@ main() {
     cargo install xargo || true
     rustup component add rust-src || true
 
-    if [ $TRAVIS_OS_NAME = osx ]; then
+    if [ $TRAVIS_OS_NAME = linux ]; then
+        sudo apt-get install --no-install-recommends -y gcc-arm-none-eabi
+    else
         brew cask install gcc-arm-embedded
     fi
 }
